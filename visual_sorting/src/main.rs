598c21355ebb3f3b18b0a6b830d3a379b881aced
jonathan-blomlof <joning.b@hotmail.com>
2022-11-28 20:03:18 +0100

    Write sorters with Vec. Now visually sorts. TODO: Test cases.

--- visual_sorting/src/main.rs
+++ visual_sorting/src/main.rs
@@ -1,12 +1,35 @@
 // The standard template taken from the git repo ggez : https://github.com/ggez/ggez
-use ggez::{Context, ContextBuilder, GameResult};
-use ggez::graphics::{self, Color};
 use ggez::event::{self, EventHandler};
-mod sorters;
+use ggez::graphics::{self, Color};
+use ggez::{Context, ContextBuilder, GameResult};
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
+mod sorters_vec;
+
+//constants from template
+//modified to only allow one elemnt to take a unique value.
+//eg. the values range from 0..vec.len()
+//so it produces a smooth line.
+const GRID_WIDTH: usize = 100;
+const GRID_HEIGHT: usize = 100;
+const GRID_CELL_SIZE: usize = 5;
+
+/// Size of the application window.
+const SCREEN_SIZE: (f32, f32) = (
+    GRID_WIDTH as f32 * GRID_CELL_SIZE as f32,
+    GRID_HEIGHT as f32 * GRID_CELL_SIZE as f32,
+);
 
 fn main() {
     // Make a Context.
-    let (mut ctx, event_loop) = ContextBuilder::new("visual_sorter", "Jonathan Blomlöf")
+    let context_builder = ggez::ContextBuilder::new("visual_sorter", "jblomlof")
+        .window_setup(ggez::conf::WindowSetup::default().title("visual_sorter"))
+        .window_mode(
+            ggez::conf::WindowMode::default()
+                .dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1)
+                .resizable(false),
+        );
+    let (mut ctx, event_loop) = context_builder
         .build()
         .expect("aieee, could not create ggez context!");
 
@@ -15,79 +38,133 @@ fn main() {
     // use when setting your game up.
     let visual_sorter = VisualSorter::new(&mut ctx);
 
-    let mut arr: [isize; 13];
-
-    arr = [1,-3,4, 15, 2, -300, 30, 400, 51, 21, 20, 21, 15];
-    sorters::insert_sort(&mut arr);
-    println!("\n");
-    print!("insert sort:\t");
-    for i in arr {
-        print!("{}, ", i);   
-    }
-
-    arr = [1,-3,4, 15, 2, -300, 30, 400, 51, 21, 20, 21, 15];
-    sorters::selection_sort(&mut arr);
-    println!("\n");
-    print!("selection sort:\t");
-    for i in arr {
-        print!("{}, ", i);   
-    }
-
-    arr = [1,-3, 4, 15, 2, -300, 30, 400, 51, 21, 20, 21, 15];
-    sorters::merge_sort(&mut arr);
-    println!("\n");
-    print!("merge sort:\t");
-    for i in arr {
-        print!("{}, ", i);   
-    }
-
-    arr = [1,-3, 4, 15, 2, -300, 30, 400, 51, 21, 20, 21, 15];
-    sorters::yeet_sort(&mut arr);
-    println!("\n");
-    print!("yeet sort:\t");
-    for i in arr {
-        print!("{}, ", i);   
-    }
-    
-    let mut _arr = [5,3,7,1,2,-2,-3,-6];
-
-    let steps_done = sorters::merge_sort(&mut _arr);
-    println!("\n");
-    for i in 0..steps_done.len() {
-        if i % _arr.len() == 0 {
-            println!("");
-        }
-        print!("{}, ", steps_done.get(i).unwrap());
-        
-    }
-
-    println!("");
     // Run!
     event::run(ctx, event_loop, visual_sorter);
 }
 
 struct VisualSorter {
     // Your state here...
+    sorting: [bool; 4],
+    sort_step: usize,
+
+    unsorted: [Vec<isize>; 4],
+
+    solution: [Vec<Vec<isize>>; 4],
+
+    sorted: [Vec<isize>; 4],
 }
 
 impl VisualSorter {
     pub fn new(_ctx: &mut Context) -> VisualSorter {
         // Load/create resources such as images here.
+        let a1 = Self::generate_random_grid();
+        let a2 = Self::generate_random_grid();
+        let a3 = Self::generate_random_grid();
+        let a4 = Self::generate_random_grid();
+        let mut sort1 = a1.to_vec();
+        let solution1 = sorters_vec::insert_sort(&mut sort1);
+        let mut sort2 = a2.to_vec();
+        let solution2 = sorters_vec::selection_sort(&mut sort2);
+        let mut sort3 = a3.to_vec();
+        let solution3 = sorters_vec::merge_sort(&mut sort3);
+        let mut sort4 = a4.to_vec();
+        let solution4 = sorters_vec::yeet_sort(&mut sort4);
+
         VisualSorter {
             // ...
+            sorting: [true, false, false, false],
+            sort_step: 0,
+            unsorted: [a1, a2, a3, a4],
+            solution: [solution1, solution2, solution3, solution4],
+            sorted: [sort1, sort2, sort3, sort4],
         }
+    }
+
+    //copied and modified from template
+    pub fn generate_random_grid() -> Vec<isize> {
+        let mut random_gen = thread_rng();
+        let mut values: Vec<isize> = (0..GRID_HEIGHT).map(|x| x as isize).collect();
+        values.shuffle(&mut random_gen);
+        values
     }
 }
 
 impl EventHandler for VisualSorter {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         // Update code here...
+        //setting current so i can easily acces what algorithm it is. eg. 0 is insertion, 1 is selection, 2 is merge, 3 is yeet
+        let current = {
+            if self.sorting[0] {
+                0
+            } else if self.sorting[1] {
+                1
+            } else if self.sorting[2] {
+                2
+            } else {
+                3
+            }
+        };
+        if self.sort_step > self.solution[current].len() + 40 {
+            // algorithm reached finish, go next.
+            self.sorting[current] = false;
+            if current != self.sorting.len() - 1 {
+                self.sorting[current + 1] = true;
+            } else {
+                self.sorting[0] = true;
+            } //start over but now we are on the next sortingalgorithm
+            self.sort_step = 0;
+        } else {
+            // we are not done yet go to next stage
+            self.sort_step += 1;
+        }
         Ok(())
     }
 
+    //everything involving drawing is inspired by GGEZ template snake game.
+    //it was really hard figuring out what to do so it helped a lot to understand how you draw the most simple shape there is
+    //but nothing was really taken, it just pointed out that canvas.draw is the way to go, but i guess you could say the whole canvas.draw() was taken
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
         // Draw code here...
-        canvas.finish(ctx)
+        let current = {
+            // same as in update
+            if self.sorting[0] {
+                0
+            } else if self.sorting[1] {
+                1
+            } else if self.sorting[2] {
+                2
+            } else {
+                3
+            }
+        };
+        if self.sort_step < self.solution[current].len() {
+            // sort_step is going to > max_index to allow for pauses to happen
+            //this protects from index out of bounds.
+            for _col in 0..GRID_WIDTH {
+                let start_y_reversed = SCREEN_SIZE.1 as i32 // i want it to sort so y= 0 is at bottom
+                    - GRID_CELL_SIZE as i32
+                    - *self.solution[current]
+                        .get(self.sort_step)
+                        .unwrap()
+                        .get(_col)
+                        .unwrap() as i32
+                        * GRID_CELL_SIZE as i32;
+                canvas.draw(
+                    &graphics::Quad,
+                    graphics::DrawParam::new()
+                        .dest_rect(graphics::Rect::new_i32(
+                            _col as i32 * GRID_CELL_SIZE as i32,
+                            start_y_reversed,
+                            GRID_CELL_SIZE as i32,
+                            SCREEN_SIZE.1 as i32 - start_y_reversed, // making em rectangles with colour all the way from bottom to value
+                        ))
+                        .color(Color::from_rgb_u32(0xDe3163)),//den är cerise, inte rosa
+                )
+            }
+            canvas.finish(ctx)?;
+            ggez::timer::yield_now();
+        }
+        Ok(())
     }
 }
---
+++ visual_sorting/src/sorters_vec.rs
@@ -0,0 +1,114 @@
+//just the same sorters as sorters.rs but with vectors
+
+///yeet_sort constantly compares two consecutive elements, if the first one is greater than the other.
+/// It yeets it to the back.
+pub fn yeet_sort(_list: &mut Vec<isize>) -> Vec<Vec<isize>> {
+    // my custom made sort. I came up with the idea my self but dont know if it already exists.
+    let mut index = 0;
+    let mut return_vec: Vec<Vec<isize>> = vec![];
+    return_vec.push(_list.to_vec());
+    while index < _list.len() - 1 {
+        if _list.get(index).unwrap() > _list.get(index + 1).unwrap() {
+            // the element thats infront is higher so we yeet it (_list[index]) to the back.
+            let temp = _list.remove(index);
+            _list.push(temp);
+            index = 0;
+            return_vec.push(_list.to_vec());
+        } else {
+            index += 1;
+        }
+    }
+    return_vec
+}
+pub fn insert_sort(_list: &mut Vec<isize>) -> Vec<Vec<isize>> {
+    let mut return_vec: Vec<Vec<isize>> = vec![];
+    return_vec.push(_list.to_vec());
+    for i in 1.._list.len() {
+        let x = _list.remove(i);
+        let mut j: isize = i as isize - 1;
+        while (j >= 0) && (*_list.get(j as usize).unwrap() > x) {
+            j -= 1;
+        }
+        _list.insert((j + 1) as usize, x);
+        if j < i as isize - 1 {
+            return_vec.push(_list.to_vec());
+        }
+    }
+    return_vec
+}
+
+pub fn selection_sort(_list: &mut Vec<isize>) -> Vec<Vec<isize>> {
+    let mut return_vec: Vec<Vec<isize>> = vec![];
+    return_vec.push(_list.to_vec());
+    for i in 0..(_list.len() - 1) {
+        let mut lowest_index = i;
+        for j in (i + 1).._list.len() {
+            if _list.get(j).unwrap() < _list.get(lowest_index).unwrap() {
+                lowest_index = j;
+            }
+        }
+        if lowest_index != i {
+            let from_switch = _list.remove(lowest_index);
+            let to_switch = _list.remove(i);
+            _list.insert(i, from_switch);
+            _list.insert(lowest_index, to_switch);
+        }
+        return_vec.push(_list.to_vec());
+    }
+    return_vec
+}
+
+pub fn merge_sort(_list: &mut Vec<isize>) ->Vec<Vec<isize>> {
+    let mut return_vec: Vec<Vec<isize>> = vec![];
+    
+    if _list.len() != 1 {
+        let n = _list.len() / 2;
+        let m = _list.len();
+        let mut second_half = _list.split_off(n);
+        let mut result = merge_sort(_list);
+        for mut i in result{
+            i.extend(second_half.as_slice());
+            return_vec.push(i.to_vec());
+        }
+        let result2 = merge_sort(&mut second_half);
+        for mut i in result2{
+            let mut temp_vec : Vec<isize> = vec![];
+            temp_vec.extend(_list.as_slice());
+            temp_vec.extend(i.as_slice());
+            return_vec.push(temp_vec.to_vec());
+        }
+        // I couldn't solve problem with borrowing twice so i got "inspired" by
+        // https://www.hackertouch.com/merge-sort-in-rust.html
+        // previous attempt commented out, but onlt in sorters.rs
+        //merge(&mut _list, &left_side, &right_side);
+        //---
+        //return_vec.extend(_list.iter());
+
+        let temp = merge(&_list, &second_half);
+        *_list = temp.to_vec();
+        return_vec.push(_list.to_vec());
+    }
+    return_vec
+}
+
+pub fn merge(_list_a: &Vec<isize>, _list_b: &Vec<isize>) -> Vec<isize> {
+    let mut left_counter = 0;
+    let mut right_counter = 0;
+    let mut return_vec = vec![];
+    while (left_counter < _list_a.len()) && (right_counter < _list_b.len()) {
+        return_vec.push(if _list_a.get(left_counter).unwrap() < _list_b.get(right_counter).unwrap() {
+            left_counter += 1;
+            *_list_a.get(left_counter - 1).unwrap()
+        } else {
+            right_counter += 1;
+            *_list_b.get(right_counter - 1).unwrap()
+        })
+    }
+    for i in left_counter.._list_a.len() {
+        return_vec.push(*_list_a.get(i).unwrap());
+    }
+    for i in right_counter.._list_b.len() {
+        return_vec.push(*_list_b.get(i).unwrap());
+    }
+    return_vec
+}
